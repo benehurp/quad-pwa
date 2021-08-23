eb64e4cc3934a2aa2ae5784167c87ce968502bdb
Bene-Hur Pessoa <benehurp@gmail.com>
2021-08-23 20:47:57 -0300

    Foi adicionado o ThemeProvider

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -2,7 +2,7 @@ import * as React from "react"
 import PropTypes from "prop-types"
 import Header from "./Header"
 import Footer from "./Footer"
-import GlobalStyles from "../../styles/global"
+import GlobalStyles from "../../themes/global"
 
 const Layout = ({ children }) => {
   return (
---
+++ src/themes/theme.js
@@ -0,0 +1,5 @@
+export default {
+  color: {
+    cor1: "teste",
+  },
+}
