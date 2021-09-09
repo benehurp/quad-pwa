1191912740289078d59eec91bbb98c50ae990e34
Bene-Hur Pessoa <benehurp@gmail.com>
2021-09-08 23:52:25 -0300

    Ajustes de responsividade do logo

--- src/components/Base/Logo/styled.js
+++ src/components/Base/Logo/styled.js
@@ -3,6 +3,12 @@ import { Link } from "gatsby"
 
 export const LogoWrapperStyled = styled.img`
   height: ${props => props.heigth};
+  width: auto;
+  display: block;
+
+  @media screen and (max-width: 1024px) {
+    height: calc(${props => props.heigth} * 0.8);
+  }
 `
 
 export const LogoLink = styled(Link)`
