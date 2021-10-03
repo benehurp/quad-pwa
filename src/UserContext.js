cd9c235c9d6886f71e91fc18fe08da011a551ac8
Bene-Hur Pessoa <benehurp@gmail.com>
2021-10-03 20:51:03 -0300

    Refatoração da função useWindowSize

--- src/UserContext.js
+++ src/UserContext.js
@@ -3,27 +3,5 @@ import React from "react"
 export const UserContext = React.createContext()
 
 export const UserStorage = ({ children }) => {
-  function useWindowSize() {
-    const [size, setSize] = React.useState([0, 0])
-
-    React.useLayoutEffect(() => {
-      function updateSize() {
-        setSize([window.innerWidth, window.innerHeight])
-      }
-      window.addEventListener("resize", updateSize)
-      updateSize()
-      return () => window.removeEventListener("resize", updateSize)
-    }, [])
-    return size
-  }
-
-  return (
-    <UserContext.Provider
-      value={{
-        useWindowSize,
-      }}
-    >
-      {children}
-    </UserContext.Provider>
-  )
+  return <UserContext.Provider value={{}}>{children}</UserContext.Provider>
 }
