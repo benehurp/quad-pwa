16d5d23e47c959ea88989bc374d35a76f81d66b8
Bene-Hur Pessoa <benehurp@gmail.com>
2021-10-04 21:22:09 -0300

    Remoção do modulo stock market

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -2,7 +2,6 @@ import * as React from "react"
 import PropTypes from "prop-types"
 import Header from "./Header"
 import Footer from "./Footer"
-import { StockMarket } from "../Base/StockMarket"
 
 const Layout = ({ children }) => {
   return (
